5d0d8cf765bcf216a90ad47e76e2b87c60cddf4f
danaamr <s-dana.marei@zewailcity.edu.eg>
2026-03-22 22:25:38 +0200

    final2 pipeline

--- check_threshold.py
+++ check_threshold.py
@@ -1,13 +1,15 @@
 import mlflow
 import sys
+import os
 
-mlflow.set_tracking_uri("file:./mlruns")
+# use secret if available
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file:mlruns"))
 
 with open("model_info.txt") as f:
     run_id = f.read().strip()
 
 run = mlflow.get_run(run_id)
-accuracy = run.data.metrics["accuracy"]
+accuracy = run.data.metrics.get("accuracy", 0)
 
 print("Accuracy:", accuracy)
 
